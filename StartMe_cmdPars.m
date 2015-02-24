f3062f225a5be58798e8f5c3698ef4f01e9241a6
Wei Wang <weicwru@gmail.com>
2015-02-24 14:47:34 -0500

    Update StartMe_cmdPars.m
    bug fixes: cannot open Viewer

--- StartMe_cmdPars.m
+++ StartMe_cmdPars.m
@@ -13,9 +13,12 @@ function StartMe_cmdPars(varargin) %varargin
         needOpenDialog = false;
         FilePath = varargin{1};
         FileName = varargin{2};
-    elseif length(varargin) > 2
-            XmlFilePath = varargin{3};
-            XmlFileName = varargin{4};
+    elseif length(varargin) == 4
+        needOpenDialog = false;
+        FilePath = varargin{1};
+        FileName = varargin{2};
+        XmlFilePath = varargin{3};
+        XmlFileName = varargin{4};
     end
     
     EDF_View({})
